bf2f496036182ce036bd0dd0135178790bbb708a
IrisDevelopment <75667602+IrisDevelopment@users.noreply.github.com>
2020-12-30 11:45:08 +1100

    Create rootfling.lua

---
+++ rootfling.lua
@@ -0,0 +1,82 @@
+plr = game.Players.LocalPlayer
+dead = false
+char = plr.Character
+
+bullet = workspace[plr.Name]["HumanoidRootPart"]
+bullet.Transparency = 0
+bhandle = bullet
+bullet.Massless = true
+bullet:FindFirstChildOfClass("Attachment"):Destroy()
+bullet:FindFirstChildOfClass("Attachment"):Destroy()
+bullet:FindFirstChildOfClass("Attachment"):Destroy()
+
+mouse = plr:GetMouse()
+head = char.Head
+camera = workspace.CurrentCamera
+lt = true
+ltt = false
+
+local function IsFirstPerson()
+     return (head.CFrame.p - camera.CFrame.p).Magnitude < 1
+end
+
+     bbv = Instance.new("BodyPosition",bhandle)
+     bbv.Position = char.Torso.CFrame.p
+   
+     
+     
+     mouse.Button1Down:Connect(function()
+         if dead == false then
+        lt = false
+        ltt = false
+     bbav = Instance.new("BodyAngularVelocity",bhandle)
+     bbav.MaxTorque = Vector3.new(math.huge,math.huge,math.huge)
+     bbav.P = 1000000000000000000000000000
+     bbav.AngularVelocity = Vector3.new(10000000000000000000000000000000,100000000000000000000000000,100000000000000000)
+     game:GetService("Debris"):AddItem(bbav,0.1)
+        if game.Players:GetPlayerFromCharacter(mouse.Target.Parent) then
+            if mouse.Target.Parent.Name == char.Name or mouse.Target.Parent.Name == "non" then return end
+              --repeat 
+            game:GetService("RunService").RenderStepped:Wait()
+            bbv.Position = (CFrame.new(mouse.Target.Parent.HumanoidRootPart.CFrame.p,char.Torso.CFrame.p) * CFrame.new(0,0,0)).p
+            bhandle.Position = (CFrame.new(mouse.Target.Parent.HumanoidRootPart.CFrame.p,char.Torso.CFrame.p) * CFrame.new(0,0,0)).p
+            wait(1)
+            --until char.Humanoid.Health == 100 or char.Humanoid.Health == 0
+        elseif game.Players:GetPlayerFromCharacter(mouse.Target.Parent.Parent) then
+            if mouse.Target.Parent.Name == char.Name or mouse.Target.Parent.Name == "non" then return end
+            --repeat 
+            game:GetService("RunService").RenderStepped:Wait()
+            bbv.Position = (CFrame.new(mouse.Target.Parent.Parent.HumanoidRootPart.CFrame.p,char.Torso.CFrame.p) * CFrame.new(0,0,0)).p
+            bhandle.Position = (CFrame.new(mouse.Target.Parent.Parent.HumanoidRootPart.CFrame.p,char.Torso.CFrame.p) * CFrame.new(0,0,0)).p
+            wait(1)
+            --until char.Humanoid.Health == 100 or char.Humanoid.Health == 0
+            
+            else
+       -- repeat 
+        game:GetService("RunService").RenderStepped:Wait()
+        bbv.Position = mouse.Hit.p
+        bhandle.Position = mouse.Hit.p
+        wait(1)
+        --until char.Humanoid.Health == 100 or char.Humanoid.Health == 0
+        end
+        wait()
+        lt = true
+         end
+         end)
+         
+     char.Humanoid.Died:Connect(function()
+         dead = true
+     end)
+     repeat 
+         game:GetService("RunService").RenderStepped:Wait()
+         if dead == false and bhandle.CanCollide == true then
+         bhandle.CanCollide = false
+         end
+         --if lt == true and dead == false then
+         --bhandle.CFrame = char.Torso.CFrame
+         bbv.Position = char.Torso.CFrame.p
+         if ltt == true and dead == false then
+        --bhandle.CFrame = char.Torso.CFrame * CFrame.new(0,0,-6)
+        bhandle.Rotation = char.Torso.Rotation
+         end
+    until char.Humanoid.Health == 0
